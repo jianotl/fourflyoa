c80859fa0b110e15bb5c594aa421e288c9e610be
zhangj <923564276@qq.com>
2021-02-07 20:41:44 +0800

    swagger配置文件

---
+++ fourfly-application/src/main/java/com/org/fourfly/config/SwaggerConfig.java
@@ -0,0 +1,40 @@
+package com.org.fourfly.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+
+@Configuration
+@EnableSwagger2
+@Profile({"development", "test"})
+public class SwaggerConfig {
+
+
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .host("localhost:8080")
+//                .pathMapping("/v2/organization/")
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.org.fourfly.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("AD-Exchange-在线API文档")
+                .description("AD-Exchange管理系统，后台测试API")
+                .version("1.0")
+                .build();
+    }
+}
