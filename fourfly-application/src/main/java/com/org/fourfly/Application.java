e0ecb026642c14a1ef2be93a44962770d195b597
zhangj <923564276@qq.com>
2021-02-07 22:27:54 +0800

    数据源配置

--- fourfly-application/src/main/java/com/org/fourfly/Application.java
+++ fourfly-application/src/main/java/com/org/fourfly/Application.java
@@ -3,13 +3,10 @@ package com.org.fourfly;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 //import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 @SpringBootApplication
-//设置指定路径过滤
-@ServletComponentScan
 public class Application extends SpringBootServletInitializer {
 
     @Override
