2e7015fd973a8f1526d11413791a387b5d39a705
jianotl <923564276@qq.com>
2021-02-12 02:23:02 +0800

    spring security 配置文件提交

---
+++ fourfly-application/src/main/java/com/org/fourfly/config/SecurityConfig.java
@@ -0,0 +1,48 @@
+package com.org.fourfly.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+/**
+ * @Author Jian
+ * @Date 2021/2/10 21:47:12
+ * @Description Spring Security 配置文件
+ * @Version 1.0
+ **/
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+        .authorizeRequests()
+        //访问"/"和"/home"路径的请求都允许
+        .antMatchers("/", "/home","/login","/login/*")
+        .permitAll()
+        //而其他的请求都需要认证
+        .anyRequest()
+        .authenticated()
+        .and()
+        //修改Spring Security默认的登陆界面
+        .formLogin()
+        .loginPage("/login")
+        .permitAll()
+        .and()
+        .logout()
+        .permitAll();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+        //基于内存来存储用户信息
+        auth.inMemoryAuthentication().passwordEncoder(new BCryptPasswordEncoder())
+                .withUser("user").password(new BCryptPasswordEncoder().encode("123")).roles("USER").and()
+                .withUser("admin").password(new BCryptPasswordEncoder().encode("456")).roles("USER","ADMIN");
+    }
+
+}
